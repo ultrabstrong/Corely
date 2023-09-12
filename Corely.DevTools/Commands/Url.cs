40630d8e83a6a522e540f102942e9bfd8864df00
Brandon <ultrabstrong@gmail.com>
2023-09-11 22:10:42 -0600

    Added url encode / decode commadns

---
+++ Corely.DevTools/Commands/Url.cs
@@ -0,0 +1,34 @@
+﻿using Corely.DevTools.Attributes;
+using Corely.Shared.Extensions;
+
+namespace Corely.DevTools.Commands
+{
+    internal class Url : CommandBase
+    {
+        [Option("-e", "--encode", Description = "Value to encode")]
+        private string Encode { get; init; }
+
+        [Option("-d", "--decode", Description = "Value to decode")]
+        private string Decode { get; init; }
+
+        public Url() : base("url", "Url encode/decode operations")
+        {
+        }
+
+        public override void Execute()
+        {
+            if (!string.IsNullOrEmpty(Encode))
+            {
+                Console.WriteLine(Encode.UrlEncode());
+            }
+            if (!string.IsNullOrEmpty(Decode))
+            {
+                Console.WriteLine(Decode.UrlDecode());
+            }
+            if (string.IsNullOrEmpty(Encode) && string.IsNullOrEmpty(Decode))
+            {
+                Console.WriteLine("No value to encode or decode");
+            }
+        }
+    }
+}
