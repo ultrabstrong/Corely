54cbcf6cbcaa0050996843b4161cc49583d4be28
Brandon <ultrabstrong@gmail.com>
2023-12-29 21:26:22 -0700

    Small console change before switching workstations

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,4 +1,10 @@
-﻿using Serilog;
+﻿using Corely.Common.Models.Security;
+using Corely.Domain;
+using Corely.Domain.Entities.Auth;
+using Corely.Domain.Mappers;
+using Corely.Domain.Models.Auth;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
 using Serilog.Core;
 
 namespace ConsoleTest
@@ -11,12 +17,18 @@ namespace ConsoleTest
         private static readonly Logger logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
 #pragma warning restore IDE0052 // Remove unread private members
 
-
         static void Main()
         {
             try
             {
-
+                var serviceProvider = CreateServiceProvider();
+                var mapper = serviceProvider.GetRequiredService<IMapProvider>();
+                mapper.Map<BasicAuthEntity>(new BasicAuth()
+                {
+                    ModifiedUtc = DateTime.UtcNow,
+                    Password = new HashedValue("password"),
+                    Username = "username"
+                });
             }
             catch (Exception ex)
             {
@@ -24,6 +36,15 @@ namespace ConsoleTest
             }
             logger.Information("Program finished. Press any key to exit.");
             Console.ReadKey();
+        }
+
+        private static ServiceProvider CreateServiceProvider()
+        {
+            var services = new ServiceCollection();
+            services.AddLogging(builder => builder.AddSerilog(logger));
+            services.AddDomainServices();
+            var serviceProvider = services.BuildServiceProvider();
+            return serviceProvider;
         }
     }
 }
