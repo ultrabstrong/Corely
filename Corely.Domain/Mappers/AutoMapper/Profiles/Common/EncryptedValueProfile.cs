d72b263816ba505fb54e9ab6ff136914c3b7d1a6
Brandon <ultrabstrong@gmail.com>
2023-12-30 21:45:55 -0700

    Added files for automapper unit tests. Still need to fill them out.

---
+++ Corely.Domain/Mappers/AutoMapper/Profiles/Common/EncryptedValueProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Corely.Common.Models.Security;
+using Corely.Domain.Mappers.AutoMapper.TypeConverters;
+
+namespace Corely.Domain.Mappers.AutoMapper.Profiles.Common
+{
+    public class EncryptedValueProfile : Profile
+    {
+        public EncryptedValueProfile()
+        {
+            CreateMap<IEncryptedValue, string>().ConvertUsing<EncryptedValueToStringTypeConverter>();
+            CreateMap<string, IEncryptedValue>().ConvertUsing<StringToEncryptedValueTypeConverter>();
+        }
+    }
+}
---
+++ Corely.UnitTests/Domain/Mappers/AutoMapper/ValueConverters/JsonifyValueConverterTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corely.UnitTests.Domain.Mappers.AutoMapper.ValueConverters
+{
+    internal class JsonifyValueConverterTests
+    {
+    }
+}
