fcc022d1ef514273aa33efb9f5ac1ffd6eb17074
Brandon <ultrabstrong@gmail.com>
2024-01-13 00:43:46 -0700

    Added mock repo tests

---
+++ Corely.UnitTests/DataAccess/Repos/Auth/MockBasicAuthRepoTests.cs
@@ -0,0 +1,76 @@
+﻿using AutoFixture;
+using Corely.DataAccess.Repos.Auth;
+using Corely.Domain.Entities.Auth;
+
+namespace Corely.UnitTests.DataAccess.Repos.Auth
+{
+    public class MockBasicAuthRepoTests
+    {
+        private readonly MockBasicAuthRepo _mockBasicAuthRepo = new();
+        private readonly Fixture _fixture = new();
+
+        public MockBasicAuthRepoTests()
+        {
+            _fixture.Customize<BasicAuthEntity>(c => c
+                .Without(x => x.User));
+        }
+
+        [Fact]
+        public async Task Create_ThenGet_ShouldAddBasicAuth()
+        {
+            var basicAuth = _fixture.Create<BasicAuthEntity>();
+
+            await _mockBasicAuthRepo.Create(basicAuth);
+            var result = await _mockBasicAuthRepo.Get(basicAuth.Id);
+
+            Assert.Equal(basicAuth, result);
+        }
+
+        [Fact]
+        public async Task Create_ThenGetByUserId_ShouldAddBasicAuth()
+        {
+            var basicAuth = _fixture.Create<BasicAuthEntity>();
+
+            await _mockBasicAuthRepo.Create(basicAuth);
+            var result = await _mockBasicAuthRepo.GetByUserId(basicAuth.UserId);
+
+            Assert.Equal(basicAuth, result);
+        }
+
+        [Fact]
+        public async Task Create_ThenGetByUserName_ShouldAddBasicAuth()
+        {
+            var basicAuth = _fixture.Create<BasicAuthEntity>();
+
+            await _mockBasicAuthRepo.Create(basicAuth);
+            var result = await _mockBasicAuthRepo.GetByUserName(basicAuth.Username);
+
+            Assert.Equal(basicAuth, result);
+        }
+
+        [Fact]
+        public async Task Create_ThenUpdate_ShouldUpdateBasicAuth()
+        {
+            var basicAuth = _fixture.Create<BasicAuthEntity>();
+
+            await _mockBasicAuthRepo.Create(basicAuth);
+            basicAuth.Username = "newUsername";
+            await _mockBasicAuthRepo.Update(basicAuth);
+            var result = await _mockBasicAuthRepo.Get(basicAuth.Id);
+
+            Assert.Equal(basicAuth, result);
+        }
+
+        [Fact]
+        public async Task Create_ThenDelete_ShouldDeleteBasicAuth()
+        {
+            var basicAuth = _fixture.Create<BasicAuthEntity>();
+
+            await _mockBasicAuthRepo.Create(basicAuth);
+            await _mockBasicAuthRepo.Delete(basicAuth);
+            var result = await _mockBasicAuthRepo.Get(basicAuth.Id);
+
+            Assert.Null(result);
+        }
+    }
+}
---
+++ Corely.UnitTests/DataAccess/Repos/User/MockUserRepoTests.cs
@@ -0,0 +1,121 @@
+﻿using AutoFixture;
+using Corely.DataAccess.Repos.User;
+using Corely.Domain.Entities.Users;
+
+namespace Corely.UnitTests.DataAccess.Repos.User
+{
+    public class MockUserRepoTests
+    {
+        private readonly MockUserRepo _mockUserRepo = new();
+        private readonly Fixture _fixture = new();
+
+        public MockUserRepoTests()
+        {
+            _fixture.Customize<UserEntity>(c => c
+                .Without(x => x.Details)
+                .Without(x => x.BasicAuth));
+        }
+
+        [Fact]
+        public async Task Create_ThenGet_ShouldAddUser()
+        {
+            var user = _fixture.Create<UserEntity>();
+
+            await _mockUserRepo.Create(user);
+            var result = await _mockUserRepo.Get(user.Id);
+
+            Assert.Equal(user, result);
+        }
+
+        [Fact]
+        public async Task Create_ThenGetByUserName_ShouldAddUser()
+        {
+            var user = _fixture.Create<UserEntity>();
+
+            await _mockUserRepo.Create(user);
+            var result = await _mockUserRepo.GetByUserName(user.Username);
+
+            Assert.Equal(user, result);
+        }
+
+        [Fact]
+        public async Task Create_ThenGetByEmail_ShouldAddUser()
+        {
+            var user = _fixture.Create<UserEntity>();
+
+            await _mockUserRepo.Create(user);
+            var result = await _mockUserRepo.GetByEmail(user.Email);
+
+            Assert.Equal(user, result);
+        }
+
+        [Fact]
+        public async Task Create_ThenGetByUserNameOrEmail_ShouldAddUser()
+        {
+            var user = _fixture.Create<UserEntity>();
+
+            await _mockUserRepo.Create(user);
+            var result = await _mockUserRepo.GetByUserNameOrEmail(user.Username, user.Email);
+
+            Assert.Equal(user, result);
+        }
+
+        [Fact]
+        public async Task Create_ThenGetWithDetailsById_ShouldAddUser()
+        {
+            var user = _fixture.Create<UserEntity>();
+
+            await _mockUserRepo.Create(user);
+            var result = await _mockUserRepo.GetWithDetailsById(user.Id);
+
+            Assert.Equal(user, result);
+        }
+
+        [Fact]
+        public async Task Create_ThenGetWithDetailsByEmail_ShouldAddUser()
+        {
+            var user = _fixture.Create<UserEntity>();
+
+            await _mockUserRepo.Create(user);
+            var result = await _mockUserRepo.GetWithDetailsByEmail(user.Email);
+
+            Assert.Equal(user, result);
+        }
+
+        [Fact]
+        public async Task Create_ThenGetWithDetailsByUserName_ShouldAddUser()
+        {
+            var user = _fixture.Create<UserEntity>();
+
+            await _mockUserRepo.Create(user);
+            var result = await _mockUserRepo.GetWithDetailsByUserName(user.Username);
+
+            Assert.Equal(user, result);
+        }
+
+        [Fact]
+        public async Task Create_ThenUpdate_ShouldUpdateUser()
+        {
+            var user = _fixture.Create<UserEntity>();
+
+            await _mockUserRepo.Create(user);
+            user.Username = _fixture.Create<string>();
+            await _mockUserRepo.Update(user);
+            var result = await _mockUserRepo.Get(user.Id);
+
+            Assert.Equal(user, result);
+        }
+
+        [Fact]
+        public async Task Create_ThenDelete_ShouldDeleteUser()
+        {
+            var user = _fixture.Create<UserEntity>();
+
+            await _mockUserRepo.Create(user);
+            await _mockUserRepo.Delete(user);
+            var result = await _mockUserRepo.Get(user.Id);
+
+            Assert.Null(result);
+        }
+    }
+}
