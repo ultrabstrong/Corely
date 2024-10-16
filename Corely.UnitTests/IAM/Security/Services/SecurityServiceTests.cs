c1d74df0f25eda9e1fbe87dfce82e6e33c5fc150
Brandon <ultrabstrong@gmail.com>
2024-10-15 22:09:27 -0600

    Improved unit tests for SecurityService

--- Corely.UnitTests/IAM/Security/Services/SecurityServiceTests.cs
+++ Corely.UnitTests/IAM/Security/Services/SecurityServiceTests.cs
@@ -1,19 +1,36 @@
 ﻿using Corely.IAM;
 using Corely.IAM.Security.Services;
 using Corely.Security.Encryption.Factories;
+using Corely.Security.Encryption.Providers;
+using Corely.Security.Signature.Factories;
+using Corely.Security.Signature.Providers;
 
 namespace Corely.UnitTests.IAM.Security.Services
 {
     public class SecurityServiceTests
     {
+        private readonly ISymmetricEncryptionProvider _symmetricEncryptionProvider;
+        private readonly IAsymmetricEncryptionProvider _asymmetricEncryptionProvider;
+        private readonly IAsymmetricSignatureProvider _asymmetricSignatureProvider;
         private readonly SecurityService _securityService;
         public SecurityServiceTests()
         {
             var serviceFactory = new ServiceFactory();
+
+            var symmetricEncryptionProviderFactory = serviceFactory.GetRequiredService<ISymmetricEncryptionProviderFactory>();
+            _symmetricEncryptionProvider = symmetricEncryptionProviderFactory.GetDefaultProvider();
+
+            var asymmetricEncryptionProviderFactory = serviceFactory.GetRequiredService<IAsymmetricEncryptionProviderFactory>();
+            _asymmetricEncryptionProvider = asymmetricEncryptionProviderFactory.GetDefaultProvider();
+
+            var asymmetricSignatureProviderFactory = serviceFactory.GetRequiredService<IAsymmetricSignatureProviderFactory>();
+            _asymmetricSignatureProvider = asymmetricSignatureProviderFactory.GetDefaultProvider();
+
             _securityService = new(
                 serviceFactory.GetRequiredService<ISecurityConfigurationProvider>(),
-                serviceFactory.GetRequiredService<ISymmetricEncryptionProviderFactory>(),
-                serviceFactory.GetRequiredService<IAsymmetricEncryptionProviderFactory>());
+                symmetricEncryptionProviderFactory,
+                asymmetricEncryptionProviderFactory,
+                asymmetricSignatureProviderFactory);
         }
 
         [Fact]
@@ -24,6 +41,12 @@ namespace Corely.UnitTests.IAM.Security.Services
             Assert.NotNull(result);
             Assert.NotNull(result.Key);
             Assert.True(result.Version > -1);
+
+            var decryptedKey = _securityService.DecryptWithSystemKey(result.Key.Secret);
+
+            Assert.True(_symmetricEncryptionProvider
+                .GetSymmetricKeyProvider()
+                .IsKeyValid(decryptedKey));
         }
 
         [Fact]
@@ -35,6 +58,12 @@ namespace Corely.UnitTests.IAM.Security.Services
             Assert.NotNull(result.PublicKey);
             Assert.NotNull(result.PrivateKey);
             Assert.True(result.Version > -1);
+
+            var decryptedPrivateKey = _securityService.DecryptWithSystemKey(result.PrivateKey.Secret);
+
+            Assert.True(_asymmetricEncryptionProvider
+                .GetAsymmetricKeyProvider()
+                .IsKeyValid(result.PublicKey, decryptedPrivateKey));
         }
 
         [Fact]
@@ -46,6 +75,12 @@ namespace Corely.UnitTests.IAM.Security.Services
             Assert.NotNull(result.PublicKey);
             Assert.NotNull(result.PrivateKey);
             Assert.True(result.Version > -1);
+
+            var decryptedPrivateKey = _securityService.DecryptWithSystemKey(result.PrivateKey.Secret);
+
+            Assert.True(_asymmetricSignatureProvider
+                .GetAsymmetricKeyProvider()
+                .IsKeyValid(result.PublicKey, decryptedPrivateKey));
         }
     }
 }
