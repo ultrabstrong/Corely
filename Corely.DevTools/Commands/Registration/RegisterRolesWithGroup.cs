992a10c067a7547a0a4a6ab2042db880d685459d
Brandon <ultrabstrong@gmail.com>
2024-12-22 23:30:43 -0700

    Add register role/group with user/group to dev tools

---
+++ Corely.DevTools/Commands/Registration/RegisterRolesWithGroup.cs
@@ -0,0 +1,56 @@
+﻿using Corely.Common.Extensions;
+using Corely.DevTools.Attributes;
+using Corely.IAM.Models;
+using Corely.IAM.Services;
+using Corely.IAM.Validators;
+using System.Text.Json;
+
+namespace Corely.DevTools.Commands.Registration;
+internal partial class Registration : CommandBase
+{
+    internal class RegisterRolesWithGroup : CommandBase
+    {
+        [Argument("Filepath to register roles with group request json", true)]
+        private string RequestJsonFile { get; init; } = null!;
+
+        [Option("-c", "--create", Description = "Create sample json file at path")]
+        private bool Create { get; init; }
+
+        private readonly IRegistrationService _registrationService;
+
+        public RegisterRolesWithGroup(IRegistrationService registrationService) : base("roles-with-group", "Register roles with group")
+        {
+            _registrationService = registrationService.ThrowIfNull(nameof(registrationService));
+        }
+
+        protected async override Task ExecuteAsync()
+        {
+            if (Create)
+            {
+                CreateSampleJson(RequestJsonFile, new RegisterRolesWithGroupRequest([1, 2], 3));
+            }
+            else
+            {
+                await RegisterRolesWithGroupAsync();
+            }
+        }
+
+        private async Task RegisterRolesWithGroupAsync()
+        {
+            var request = ReadRequestJson<RegisterRolesWithGroupRequest>(RequestJsonFile);
+            if (request == null) return;
+            try
+            {
+                foreach (var registerRequest in request)
+                {
+                    var result = await _registrationService.RegisterRolesWithGroupAsync(registerRequest);
+                    Console.WriteLine(JsonSerializer.Serialize(result));
+                }
+            }
+            catch (ValidationException ex)
+            {
+                Error(ex.ValidationResult!.Errors!.Select(e => e.Message));
+            }
+        }
+    }
+}
