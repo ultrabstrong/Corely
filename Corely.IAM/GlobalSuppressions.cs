0a3be8e833194b1f5e67ba397ec52b84362f61fd
Brandon <ultrabstrong@gmail.com>
2025-03-29 22:56:10 -0600

    Uncomment IAM global suppression for primary constructor

--- Corely.IAM/GlobalSuppressions.cs
+++ Corely.IAM/GlobalSuppressions.cs
@@ -3,4 +3,6 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-//[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "Primary constructors don't support making params readonly (as of 12-30-23)", Scope = "module")]
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE0290:Use primary constructor", Justification = "Primary constructors don't support making params readonly (as of 12-30-23)", Scope = "module")]
